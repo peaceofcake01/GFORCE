35e41aed931bd45f7142a38ea6705e7d69edefef
peaceofcake01 <peaceofcake01@users.noreply.github.com>
2019-09-13 00:17:56 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 //set up function that will be used later
 float get_change(void);
 
@@ -15,7 +16,8 @@ int main(void)
     }
     while (n < 0);
     //set up variables that will make future steps easier by multiplying the float and making it into an integer
-    int newN = n * 100;
+    float newN = n * 100;
+    newN = roundf(newN);
     //initialize total coins to 0
     int totalCoins = 0;
     //if the total is more than 25, add 1 to total coins due to the quarter that would take its spot, and subtract 25 from the newN
@@ -24,23 +26,25 @@ int main(void)
         totalCoins++;
         newN -= 25;
     }
+   
      //if the total is more than 10, add 1 to total coins due to the dime that would take its spot, and subtract 10 from the newN
-    while (newN>=10)
+    while (newN >= 10)
     {
-        totalCoins = totalCoins+1;
-        newN = newN-10;
+        totalCoins ++;
+        newN -= 10;
     }
+    
       //if the total is more than 5, add 1 to total coins due to the nickel that would take its spot, and subtract 5 from the newN
-     while (newN>=5)
+     while (newN >= 5)
     {
-        totalCoins = totalCoins+1;
-        newN = newN-5;
+        totalCoins ++;
+        newN -= 5;
     }
       //if the total is more than 1, add 1 to total coins due to the penny that would take its spot, and subtract 1 from the newN
-     while (newN>=1)
+     while (newN >= 1)
     {
-        totalCoins = totalCoins+1;
-        newN = newN-1;
+        totalCoins ++;
+        newN -= 1;
     }
     //print the integer of minimum total coins that can be found
     printf("%i\n", totalCoins);
