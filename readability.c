06eae4b696edf7a043510c03f1f3d9ab71d4894c
peaceofcake01 <peaceofcake01@users.noreply.github.com>
2019-09-19 00:25:49 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -9,7 +9,7 @@ int main (void){
     // prompt user for string
       string s = get_string("Text:  ");
       // create variable
-      int letters = 0;
+      float letters = 0;
 
 //Find l
 for (int i = 0; i < strlen(s); i++)
@@ -19,11 +19,11 @@ for (int i = 0; i < strlen(s); i++)
         letters++;
     }
 }
-//printf("Letters: %i\n", letters);
+printf("Letters: %f\n", letters);
 
 
-    int words = 0;
-    bool flag = true;
+    float words = 0;
+    bool flag = false;
 
 
 //Find l
@@ -34,18 +34,18 @@ for (int i = 0; i < strlen(s); i++)
     {
         flag = true;
         words++;
-    }else if(!isalpha(s[i]))
+    }else if(s[i]==' ')
     {
         flag = false;
     }
 
 
 }
-//printf("Words: %i\n", words);
+printf("Words: %f\n", words);
 
 
 
-    int sentences = 0;
+    float sentences = 0;
 
 for (int i = 0; i < strlen(s); i++)
 {
@@ -56,24 +56,30 @@ for (int i = 0; i < strlen(s); i++)
 
 }
 
-//printf("Sentences: %i\n", sentences);
+printf("Sentences: %f\n", sentences);
 
-float L = 100 * letters/words;
-float S = 100 * sentences/words;
+float L = 100.0 * letters/words;
+float S = 100.0 * sentences/words;
 
+printf("L: %f\n", L);
+printf("S: %f\n", S);
 
 
 //Find initial grade level estimation
    float initialgrade = 0.0588 * L - 0.296 * S - 15.8;
 // Round the grade level
-   int finalgrade= (int) roundf(initialgrade)
-;
+printf("initialgrade: %f\n", initialgrade);
+
+int finalgrade= round(initialgrade);
+
+printf("finalgrade: %i\n", finalgrade);
 //If else statement saying that if grade ends up being larger than 16 then print grade 16+, if grade is less than 1 print before grade 1
 
     if (finalgrade>16)
     {
     printf("Grade 16+\n");
     }
+
     else if (finalgrade<1)
     {
         printf("Before Grade 1...\n");
