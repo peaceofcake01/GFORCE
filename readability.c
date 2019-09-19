ac00aac4ad26eb917b48fd268866d013f8ff6213
peaceofcake01 <peaceofcake01@users.noreply.github.com>
2019-09-19 01:53:36 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -5,82 +5,86 @@
 #include <string.h>
 #include <math.h>
 
-int main (void){
-    // prompt user for string
-      string s = get_string("Text:  ");
-      // create variable
-      float letters = 0;
-
-//Find l
-for (int i = 0; i < strlen(s); i++)
+int main(void)
 {
-    if (isalpha(s[i]))
+    // prompt user for string
+    string s = get_string("Text:  ");
+    // create variable
+    float letters = 0;
+
+//Find letters- create a loop that counts everytime that a letter of the alphabet is seen in the text.
+    for (int i = 0; i < strlen(s); i++)
     {
-        letters++;
+        if (isalpha(s[i]))
+        {
+            letters++;
+        }
     }
-}
 //printf("Letters: %f\n", letters);
 
-
+//Set variables of words and flags
     float words = 0;
     bool flag = false;
 
 
-//Find l
-for (int i = 0; i < strlen(s); i++)
-{
+//Find words, create a loop and a flag that will only change to true when a new first character is introduced
 
-    if (isalpha(s[i]) && !flag)
+    for (int i = 0; i < strlen(s); i++)
     {
-        flag = true;
-        words++;
-    }else if(s[i]==' ')
-    {
-        flag = false;
+
+        if (isalpha(s[i]) && !flag)
+        {
+            flag = true;
+            words++;
+        }
+        else if (s[i] == ' ')
+        {
+            flag = false;
+        }
+
+
     }
-
-
-}
 //printf("Words: %f\n", words);
 
 
-
+//set sentences to 0
     float sentences = 0;
 
-for (int i = 0; i < strlen(s); i++)
-{
-    if (s[i]=='.' || s[i]=='?' || s[i] =='!')
+//find the number of sentences by checking when a period, exclamation mark or question mark are hit
+    for (int i = 0; i < strlen(s); i++)
     {
-        sentences++;
-    }
+        if (s[i] == '.' || s[i] == '?' || s[i] == '!')
+        {
+            sentences++;
+        }
 
-}
+    }
 
 //printf("Sentences: %f\n", sentences);
 
-float L = 100.0 * letters/words;
-float S = 100.0 * sentences/words;
+    float L = 100.0 * letters / words;
+    float S = 100.0 * sentences / words;
 
 //printf("L: %f\n", L);
 //printf("S: %f\n", S);
 
 
 //Find initial grade level estimation
-   float initialgrade = 0.0588 * L - 0.296 * S - 15.8;
+    float initialgrade = 0.0588 * L - 0.296 * S - 15.8;
 // Round the grade level
 //printf("initialgrade: %f\n", initialgrade);
 
-int finalgrade= round(initialgrade);
+    int finalgrade = round(initialgrade);
 
 //printf("finalgrade: %i\n", finalgrade);
 //If else statement saying that if grade ends up being larger than 16 then print grade 16+, if grade is less than 1 print before grade 1
 
-    if (finalgrade>16)
+    if (finalgrade > 16)
     {
-    printf("Grade 16+\n");
+        printf("Grade 16+\n");
     }
 
-    else if (finalgrade<1)
+    else if (finalgrade < 1)
     {
         printf("Before Grade 1\n");
     }
