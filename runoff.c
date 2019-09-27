e7e92831d6adc7315d83cf545ac22b521bc86747
peaceofcake01 <peaceofcake01@users.noreply.github.com>
2019-09-27 22:19:01 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -149,18 +149,17 @@ void tabulate(void)
             int cand_ind = preferences[i][j];
             if (candidates[cand_ind].eliminated == false)
             {
-            candidates[cand_ind].votes++;
+                candidates[cand_ind].votes++;
             }
         }
 
-    return;
 }
 }
 
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    int winner = voter_count/2;
+    int winner = voter_count / 2;
 
     for (int i=0; i < candidate_count; i++)
     {
