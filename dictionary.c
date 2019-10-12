662a960d1057a4e2f99b1808e97dd47f880af21d
peaceofcake01 <peaceofcake01@users.noreply.github.com>
2019-10-12 21:35:31 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -29,25 +29,22 @@ node *table[N];
 
 bool check(const char *word)
 {
-    //Start on first element of linked list?
+    //Start on first element of linked list
     //pointer to node
 
     node *cursor;
     int n = hash(word);
     cursor = table [n];
-        while(cursor != NULL)
+    //while statement that will compare the word of the dictionary to what cursor is pointing to
+    while (cursor != NULL)
+    {
+        if (strcasecmp(cursor -> word, word) == 0)
         {
-
-            if(strcasecmp(cursor -> word, word) == 0)
-            {
-                return true;
-            }
-
-        cursor = cursor -> next;
-
+            return true;
         }
+        cursor = cursor -> next;
+    }
     return false;
-
 }
 
 //hash word to obtain hash value
@@ -57,30 +54,30 @@ bool check(const char *word)
 //Just want to convert string into number
 unsigned int hash(const char *word)
 {
-    return tolower(word[0])-'a';
+    return tolower(word[0]) - 'a';
 }
 
 int counter = 0;
 char buffer[LENGTH + 1];
-int x=0;
+int x = 0;
 
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
 
     //Take new node and put it inside hash table
-     node *list = NULL;
-     FILE *DICT = fopen(dictionary, "r");
-     while (fscanf(DICT, "%s", buffer) != EOF)
-     {
+    node *list = NULL;
+    FILE *DICT = fopen(dictionary, "r");
+    while (fscanf(DICT, "%s", buffer) != EOF)
+    {
         if (DICT == NULL)
         {
             printf("Need a Readable File\n");
             return false;
         }
 
-    //Hash the word - give it hash key-  0,1 , 2,
-    //table
+        //Hash the word - give it hash key-  0,1 , 2,
+        //table
 
         x = hash(buffer);
 
@@ -90,28 +87,25 @@ bool load(const char *dictionary)
         strcpy(n->word, buffer);
         n->next = NULL;
 
-    //add new node to head
+        //add new node to head
         n->next = list;
         table[x] = n;
 
         counter++;
 
-        }
+    }
 
-        fclose(DICT);
-         return true;
+    fclose(DICT);
+    return true;
 }
 
-    //first argument is pointer, want to read line by line ,fscanf(pointer, percent s, buffer)
-
-
-    //Linked lists- every node has value and pointer to next node
-
+//first argument is pointer, want to read line by line ,fscanf(pointer, percent s, buffer)
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
+
 unsigned int size(void)
 {
-    if(counter!=0)
+    if (counter != 0)
     {
         return counter;
     }
@@ -126,72 +120,18 @@ unsigned int size(void)
 bool unload(void)
 {
     node *pt;
-    for(int n=0; n<26; n++)
+    for (int n = 0; n < 26; n++)
     {
-       pt = table[n];
-       while(pt != NULL)
-       {
-        node *temp = pt;
-        pt = pt ->next;
-        free (temp);
-       }
+        pt = table[n];
+        while (pt != NULL)
+        {
+            node *temp = pt;
+            pt = pt -> next;
+            free(temp);
+        }
 
     }
 
     return true;
 }
 
-
-// linked list is a string dangling off a  cliff
-
-// Only pointer if you free any of the top then you lose it
-
-// How to free a linked list- can have pointer
-
-
-
-// Can go recursively
-
-// //Free
-    // node *ptr = list;
-    // while (ptr != NULL)
-    //     {
-    //         node *tmp = ptr;
-    //         ptr = ptr->next;
-    //         free(tmp);
-
-
-// when test small.txt small.txt
-
-
-
-//or can use
-    // node *n=malloc
-    // fsan(ptr,"%s", n -> word)
-    //Take buffer
-
-
-// TODO
-
-    //dictionary- words on each line
-
-    //Function is taking in a string
-    //use
-
-
-    //Store in Hashtable
-    //Hashtable is an array of linked list
-    //Hash function assigns number to every input
-    //Create different Buckets
-
-    //A bucket for all words with letter A
-    //B bucket for all with b
-
-    //Hash function takes a words as an input
-    //Outputs number corresponding to bucket it was put in
-
-    // //  node *n = malloc(sizeof(node));
-    // n->word = 25;
-    // n->next = NULL
-
-   // strcpy(n->word, buffer)
